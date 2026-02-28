539d0d1a51a1c519a385e3d98d88ff71d78871b8
jonathan <112445400+StealthAIPro@users.noreply.github.com>
2026-02-28 11:56:43 -0600

    Update uv.sw.js

--- uv.sw.js
+++ uv.sw.js
@@ -1,3 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+
 /* This file goes in your ROOT folder */
 importScripts('/uv/uv.bundle.js');
 importScripts('/uv.config.js');
